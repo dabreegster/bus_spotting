ea92841cc0bd83545ad5d6a17f665741c0cf37a9
Dustin Carlino <dabreegster@gmail.com>
2022-05-15 17:55:33 +0100

    Some notes

--- src/model/gtfs/mod.rs
+++ src/model/gtfs/mod.rs
@@ -18,3 +18,18 @@ impl GTFS {
         })
     }
 }
+
+// TODO are routes one or both directions? (probably both)
+// TODO is block_id a (useful) hint of the vehicle mapping?
+
+// TODO next steps:
+// - assign cheap numeric IDs to everything (or at least the things in World)
+// - routes: ID -> metadata
+// - trips: ID -> route, shape, direction
+// - stop_times: trip -> [stop ID, arrival time, departure time]
+// - shapes: ID -> polyline
+//
+// then group "route patterns" or "trip groups"
+// - per route, group by [stop IDs]
+// - check if those always share a shape?
+// - in practice, how many patterns per route? just directional and express/local
