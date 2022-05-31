da384579733352dab0423f202e6d9475e5d9aac3
dabreegster <dabreegster@users.noreply.github.com>
2022-05-31 13:45:54 +0000

    deploy: 0a775c0204df701f3c98bb2913236445b5b2fa5c

--- model/src/gtfs/shapes.rs
+++
@@ -1,6 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
-pub struct ShapeID(String);
-
-// TODO The sample dataset has shape_ids, but no shapes.txt
--- run_locally.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-cd ui
-wasm-pack build --dev --target web -- --no-default-features --features wasm
-cd pkg
-rm -f index.html
-ln -s ../index.html .
-python3 -m http.server 8000
