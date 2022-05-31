998f77a15520c860658406539924161ccc220243
dabreegster <dabreegster@users.noreply.github.com>
2022-05-31 13:56:49 +0000

    deploy: 26890e3e4553aeeda36299b3791fae92e3750f6a

---
+++ ui.d.ts
@@ -0,0 +1,153 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export function run_wasm(): void;
+/**
+*/
+export class IntoUnderlyingByteSource {
+  free(): void;
+/**
+* @param {any} controller
+*/
+  start(controller: any): void;
+/**
+* @param {any} controller
+* @returns {Promise<any>}
+*/
+  pull(controller: any): Promise<any>;
+/**
+*/
+  cancel(): void;
+/**
+* @returns {number}
+*/
+  readonly autoAllocateChunkSize: number;
+/**
+* @returns {any}
+*/
+  readonly type: any;
+}
+/**
+*/
+export class IntoUnderlyingSink {
+  free(): void;
+/**
+* @param {any} chunk
+* @returns {Promise<any>}
+*/
+  write(chunk: any): Promise<any>;
+/**
+* @returns {Promise<any>}
+*/
+  close(): Promise<any>;
+/**
+* @param {any} reason
+* @returns {Promise<any>}
+*/
+  abort(reason: any): Promise<any>;
+}
+/**
+*/
+export class IntoUnderlyingSource {
+  free(): void;
+/**
+* @param {any} controller
+* @returns {Promise<any>}
+*/
+  pull(controller: any): Promise<any>;
+/**
+*/
+  cancel(): void;
+}
+/**
+* Raw options for [`pipeTo()`](https://developer.mozilla.org/en-US/docs/Web/API/ReadableStream/pipeTo).
+*/
+export class PipeOptions {
+  free(): void;
+/**
+* @returns {boolean}
+*/
+  readonly preventAbort: boolean;
+/**
+* @returns {boolean}
+*/
+  readonly preventCancel: boolean;
+/**
+* @returns {boolean}
+*/
+  readonly preventClose: boolean;
+/**
+* @returns {AbortSignal | undefined}
+*/
+  readonly signal: AbortSignal | undefined;
+}
+/**
+*/
+export class QueuingStrategy {
+  free(): void;
+/**
+* @returns {number}
+*/
+  readonly highWaterMark: number;
+}
+/**
+* Raw options for [`getReader()`](https://developer.mozilla.org/en-US/docs/Web/API/ReadableStream/getReader).
+*/
+export class ReadableStreamGetReaderOptions {
+  free(): void;
+/**
+* @returns {any}
+*/
+  readonly mode: any;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly run_wasm: () => void;
+  readonly __wbg_readablestreamgetreaderoptions_free: (a: number) => void;
+  readonly readablestreamgetreaderoptions_mode: (a: number) => number;
+  readonly __wbg_pipeoptions_free: (a: number) => void;
+  readonly pipeoptions_preventClose: (a: number) => number;
+  readonly pipeoptions_preventCancel: (a: number) => number;
+  readonly pipeoptions_preventAbort: (a: number) => number;
+  readonly pipeoptions_signal: (a: number) => number;
+  readonly __wbg_intounderlyingsource_free: (a: number) => void;
+  readonly intounderlyingsource_pull: (a: number, b: number) => number;
+  readonly intounderlyingsource_cancel: (a: number) => void;
+  readonly __wbg_intounderlyingsink_free: (a: number) => void;
+  readonly intounderlyingsink_write: (a: number, b: number) => number;
+  readonly intounderlyingsink_close: (a: number) => number;
+  readonly intounderlyingsink_abort: (a: number, b: number) => number;
+  readonly __wbg_intounderlyingbytesource_free: (a: number) => void;
+  readonly intounderlyingbytesource_type: (a: number) => number;
+  readonly intounderlyingbytesource_autoAllocateChunkSize: (a: number) => number;
+  readonly intounderlyingbytesource_start: (a: number, b: number) => void;
+  readonly intounderlyingbytesource_pull: (a: number, b: number) => number;
+  readonly intounderlyingbytesource_cancel: (a: number) => void;
+  readonly __wbg_queuingstrategy_free: (a: number) => void;
+  readonly queuingstrategy_highWaterMark: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_export_2: WebAssembly.Table;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h949f5adf7c21397f: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hf795f58e1461b57f: (a: number, b: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h2443bd426ae53cb7: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h3a2d6c6a1e1be2c2: (a: number, b: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hae433a0210d72e8b: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke2_mut__h5b1a688489de8c3e: (a: number, b: number, c: number, d: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ ui_bg.wasm.d.ts
@@ -0,0 +1,37 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function run_wasm(): void;
+export function __wbg_readablestreamgetreaderoptions_free(a: number): void;
+export function readablestreamgetreaderoptions_mode(a: number): number;
+export function __wbg_pipeoptions_free(a: number): void;
+export function pipeoptions_preventClose(a: number): number;
+export function pipeoptions_preventCancel(a: number): number;
+export function pipeoptions_preventAbort(a: number): number;
+export function pipeoptions_signal(a: number): number;
+export function __wbg_intounderlyingsource_free(a: number): void;
+export function intounderlyingsource_pull(a: number, b: number): number;
+export function intounderlyingsource_cancel(a: number): void;
+export function __wbg_intounderlyingsink_free(a: number): void;
+export function intounderlyingsink_write(a: number, b: number): number;
+export function intounderlyingsink_close(a: number): number;
+export function intounderlyingsink_abort(a: number, b: number): number;
+export function __wbg_intounderlyingbytesource_free(a: number): void;
+export function intounderlyingbytesource_type(a: number): number;
+export function intounderlyingbytesource_autoAllocateChunkSize(a: number): number;
+export function intounderlyingbytesource_start(a: number, b: number): void;
+export function intounderlyingbytesource_pull(a: number, b: number): number;
+export function intounderlyingbytesource_cancel(a: number): void;
+export function __wbg_queuingstrategy_free(a: number): void;
+export function queuingstrategy_highWaterMark(a: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h949f5adf7c21397f(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hf795f58e1461b57f(a: number, b: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h2443bd426ae53cb7(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h3a2d6c6a1e1be2c2(a: number, b: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hae433a0210d72e8b(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h5b1a688489de8c3e(a: number, b: number, c: number, d: number): void;
+export function __wbindgen_start(): void;
